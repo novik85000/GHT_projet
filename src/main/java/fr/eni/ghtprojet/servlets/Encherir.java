0b32e5461b6d46b0cf7b33b5425be576d8c32222
Tatiana NOVIK <novik85000@gmail.com>
2022-03-04 16:42:30 +0100

    update

--- src/main/java/fr/eni/ghtprojet/servlets/Encherir.java
+++ src/main/java/fr/eni/ghtprojet/servlets/Encherir.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Encherir
  */
-@WebServlet("/Encherir")
+@WebServlet("/encherir")
 public class Encherir extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
